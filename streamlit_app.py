90442626601a47e8812fb3dccf6f2d4725084dff
Chanin Nantasenamat <51851491+dataprofessor@users.noreply.github.com>
2023-01-25 19:52:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ st.write('''
 ##### *Resume* 
 ''')
 
-image = Image.open('user.png')
+image = Image.open('photo.png')
 st.image(image, width=150)
 
 st.markdown('## Summary', unsafe_allow_html=True)
